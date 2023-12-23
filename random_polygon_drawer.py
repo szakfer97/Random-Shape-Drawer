e9b81eb9914e614c1504c0de1476e1520c2f138a
szakferi <szakfer97@gmail.com>
2023-12-23 16:27:19 +0200

    Added comments to programs

--- random_polygon_drawer.py
+++ random_polygon_drawer.py
@@ -1,23 +1,37 @@
 import turtle
 import random
 
+# Function to draw a polygon with a given number of sides
+
 
 def draw_polygon(noSides):
+    # Calculate the internal and external angles of the polygon
     internalAngles = ((noSides - 2) * 180) / noSides
     externalAngles = 180 - internalAngles
+
+    # Draw the polygon
     for i in range(noSides):
         my_pen.forward(100)
-        my_pen.left(externalAngles),
+        my_pen.left(externalAngles)
+
+    # Adjust the turtle's orientation for the next polygon
     my_pen.right(externalAngles)
 
 
+# Set up turtle screen
 tut = turtle.Screen()
 tut.bgcolor("black")
-tut.title("Continous random drawing of polygons with sides from 3 to 10")
+tut.title("Continuous random drawing of polygons with sides from 3 to 10")
+
+# Create a turtle
+
 my_pen = turtle.Turtle()
 my_pen.color("white")
-tut = turtle.Screen()
+
+# Draw 30 polygons with sides randomly chosen between 3 and 10
 
 for i in range(30):
     draw_polygon(random.randint(3, 10))
+
+# Finish drawing
 turtle.done()
